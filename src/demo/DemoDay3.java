c97e603c352442713ea1e66b51bc6a9b1add5d4a
sterling23-madrid <120076006+sterling23-madrid@users.noreply.github.com>
2023-08-03 19:49:20 -0400

    added a conflict

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -9,7 +9,7 @@ public class DemoDay3 {
 	public static void main(String[] args) {
 		
 
-		System.out.println("Tiffany conflict");
+		System.out.println("Sterling conflict");
 
 		
 		System.out.println("********Eltaf Sameem********");
